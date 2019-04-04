1a07272538532c74dd99526a7cc515b304d5ac81
Dan Lester <dan@danlester.com>
2019-04-04 14:26:18 +0100

    Downloads cleaned up

---
+++ config_sample.py
@@ -0,0 +1,6 @@
+# Rename this file to config.py
+# And obtain your own API Key and Secret from Flickr here: https://www.flickr.com/services/api/misc.api_keys.html
+# Fill in the strings below with your values
+API_KEY = '<API KEY STRING>'
+API_SECRET = '<SECRET STRING>'
+
